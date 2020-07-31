b445eee132f71d0db260c1f9d0e37de67ef38e75
Litleck <nicholasowens699@gmail.com>
2020-07-30 22:19:57 -0400

    Added snippets and other minor changes.

--- src/commands/utility/info.js
+++ src/commands/utility/info.js
@@ -9,7 +9,7 @@ module.exports = {
       .setColor(successColor)
       .setThumbnail(client.user.displayAvatarURL())
       .setTitle("Minkinator Information")
-      .setDescription("Minkinator is a one of the kind ~~nightmare~~ friend that will help you along your journeys. He is the digitalized form of the rat god, Minky. Allow Minkinator to ~~take control of your life~~ help you out whenever the time comes.")
+      .setDescription("What is a Minkinator you may ask. Well, I'm not quite sure myself. On the 22nd of August, 2019, I just decided to try and make a Discord bot on my phone. Soon Minkinator would be hosted on my desktop computer and currently is hosted on my own Raspberry Pi. I never had any idea that Minkinator would reach this far. If you ever find a bug or have a feature request, please do so on the GitHub repository below.")
       .addField("Commands:", client.commands.size, true)
       .addField("Events:", client.events.size, true)
       .addField("GitHub:", "https://github.com/Litleck/Minkinator")
