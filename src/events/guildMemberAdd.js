f5b34f5de775a0bb2b27272540fd714a160a2184
Litleck <nicholasowens699@gmail.com>
2020-08-29 00:26:17 -0400

    Fixed join / leave messages.

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,26 +1,33 @@
 module.exports = async (client, member) => {
-  const channel = member.guild.channels.cache.find(channel => channel.name === "member-log");
-  const database = client.databases[member.guild.name];
-  const time = client.moment().format("HH:mm M/D/Y");
+  const guild = member.guild;
+  const channel = guild.channels.cache.find(channel => channel.name === "member-log");
+
+  if (!channel) return;
+
+  const database = client.databases[guild.name];
   const pluralize = client.pluralize;
+
+  const time = client.moment().format("HH:mm M/D/Y");
+
+  const guildConfig = await database.properties.findByPk("configuration").then(key => key.value);
+  const defaultColor = guildConfig.colors.default;
 
   // Set count values
   const users = pluralize("user", client.users.cache.size, true);
   const guilds = pluralize("guild", client.guilds.cache.size, true);
 
-  // Set user activity
+  // Update bot activity
   client.user.setActivity(`${users} in ${guilds}.`, { type: "WATCHING" });
 
-  console.log(`${`(${time})`.green} ${member.user.tag} has joined ${member.guild.name}.`);
+  console.log(`${`(${time})`.green} ${member.user.tag} has joined ${guild.name}.`);
 
+  // Create member data
   await database.members.create({ name: member.user.tag, id: member.id });
 
   // Send embed
-  if (channel) {
-    return channel.send(new client.Discord.MessageEmbed()
-      .setColor(client.config.embed.color)
-      .setAuthor(`${member.user.tag} (${member.id})`, member.user.avatarURL())
-      .setFooter("User joined")
-    );
-  }
+  return channel.send(new client.Discord.MessageEmbed()
+    .setColor(defaultColor)
+    .setAuthor(`${member.user.tag} (${member.id})`, member.user.displayAvatarURL())
+    .setFooter("User joined")
+  );
 };
