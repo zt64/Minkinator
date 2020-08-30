a48c88a2cd2b9d802e814e60843685d2e5e64abb
Litleck <nicholasowens699@gmail.com>
2020-08-30 00:28:56 -0400

    Added scale command.

---
+++ src/commands/canvas/scale.js
@@ -0,0 +1,37 @@
+module.exports = {
+  description: "Change an images scale.",
+  parameters: [
+    {
+      name: "url",
+      type: String,
+      required: true
+    },
+    {
+      name: "x factor",
+      type: Number,
+      required: true
+    },
+    {
+      name: "y factor",
+      type: Number,
+      required: true
+    }
+  ],
+  async execute (client, message, args) {
+    const imageURL = args[0];
+    const image = await client.canvas.loadImage(imageURL);
+
+    const xFactor = parseFloat(args[1]);
+    const yFactor = parseFloat(args[2]);
+
+    const canvas = client.canvas.createCanvas(image.width * xFactor, image.height * yFactor);
+    const context = canvas.getContext("2d");
+
+    context.scale(xFactor, yFactor);
+    context.drawImage(image, 0, 0);
+
+    const attachment = new client.Discord.MessageAttachment(canvas.toBuffer());
+
+    return message.channel.send(attachment);
+  }
+};
