511466ef9517a1b555bbcc0a863446a8045693d7
Litleck <nicholasowens699@gmail.com>
2020-09-15 18:06:32 -0400

    Added slowmode command.

---
+++ src/commands/admin/slowmode.js
@@ -0,0 +1,18 @@
+module.exports = {
+  description: "Set the current channels slow mode.",
+  aliases: ["slow"],
+  parameters: [
+    {
+      name: "seconds",
+      type: Number,
+      required: true
+    }
+  ],
+  async execute (client, message, [ seconds ]) {
+    const channel = message.channel;
+
+    await channel.setRateLimitPerUser(seconds);
+
+    return message.channel.send(`Successfully set the rate limit per user to ${seconds} seconds.`);
+  }
+};
