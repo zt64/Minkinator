cc43b735137227c6727ad765ab8eaede729b00b7
Litleck <nwowens32@gmail.com>
2020-12-07 17:54:23 -0500

    Fixed issue when members join or leave guilds

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,29 +1,20 @@
-module.exports = async (client, member) => {
-  const { guild } = member;
+module.exports = async (client, { guild, user }) => {
   const channel = guild.channels.cache.find(channel => channel.name === "member-log");
-
   if (!channel) return;
 
   const { pluralize, chalk } = global;
+  const { config } = await global.sequelize.models.guild.findByPk(guild.id, { include: "config" });
 
-  const time = global.moment().format("HH:mm M/D/Y");
-
-  const guildConfig = await global.sequelize.models.guild.findByPk(guild.id, { include: "config" });
-  const defaultColor = guildConfig.colors.default;
-
-  // Set count values
   const users = pluralize("user", client.users.cache.size, true);
   const guilds = pluralize("guild", client.guilds.cache.size, true);
-
-  // Update bot activity
   client.user.setActivity(`${users} in ${guilds}.`, { type: "WATCHING" });
 
-  console.log(chalk.green(`(${time})`), `${member.user.tag} has joined ${guild.name}`);
+  const time = global.moment().format("HH:mm M/D/Y");
+  console.log(chalk.green(`(${time})`), `${user.tag} has joined ${guild.name}.`);
 
-  // Send embed
-  return channel.send(new global.Discord.MessageEmbed()
-    .setColor(defaultColor)
-    .setAuthor(`${member.user.tag} (${member.id})`, member.user.displayAvatarURL())
-    .setFooter("User joined")
-  );
+  return channel.send(new global.Discord.MessageEmbed({
+    color: config.colors.default,
+    author: { iconURL: user.displayAvatarURL(), name: user.tag },
+    footer: { text: "User Joined" }
+  }));
 };
--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -1,34 +1,24 @@
-module.exports = async (client, member) => {
-  const { guild } = member;
+module.exports = async (client, { guild, user }) => {
   const channel = guild.channels.cache.find(channel => channel.name === "member-log");
-
   if (!channel) return;
 
   const { pluralize, chalk } = global;
-
-  const time = global.moment().format("HH:mm M/D/Y");
-
   const { models } = global.sequelize;
+  const { config } = await models.guild.findByPk(guild.id, { include: "config" });
 
-  const guildInstance = await models.guild.findByPk(guild.id, { include: "config" });
-  const defaultColor = guildInstance.config.colors.default;
-
-  // Set count values
   const users = pluralize("user", client.users.cache.size, true);
   const guilds = pluralize("guild", client.guilds.cache.size, true);
-
-  // Update bot activity
   client.user.setActivity(`${users} in ${guilds}.`, { type: "WATCHING" });
+  
+  const data = await models.member.findByPk(user.id);
+  if (data) await data.destroy();
 
-  console.log(chalk.green(`(${time})`), `${member.user.tag} has left ${guild.name}`);
+  const time = global.moment().format("HH:mm M/D/Y");
+  console.log(chalk.green(`(${time})`), `${user.tag} has left ${guild.name}.`);
 
-  // Destroy member data
-  await models.member.findByPk(member.id).then(data => data.destroy());
-
-  // Send embed
-  return channel.send(new global.Discord.MessageEmbed()
-    .setColor(defaultColor)
-    .setAuthor(`${member.user.tag} (${member.id})`, member.user.displayAvatarURL())
-    .setFooter("User left")
-  );
+  return channel.send(new global.Discord.MessageEmbed({
+    color: config.colors.default,
+    author: { iconURL: user.displayAvatarURL(), name: user.tag },
+    footer: { text: "User Left" }
+  }));
 };
