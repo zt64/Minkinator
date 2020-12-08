6b5c28ff144e6de157ded472ba69863988993266
Litleck <nwowens32@gmail.com>
2020-12-07 22:46:02 -0500

    Switched QR code command to use online API

--- src/commands/utility/qr.js
+++ src/commands/utility/qr.js
@@ -1,4 +1,5 @@
-const qr = require("qrcode");
+const fetch = require("node-fetch");
+const entities = require("entities");
 
 module.exports = {
   description: "Generates a QR code using the input.",
@@ -9,19 +10,17 @@ module.exports = {
     }
   ],
   async execute (client, message, args) {
-    const { colors } = global.guildInstance.config;
-    const input = args.join(" ");
+    const text = args.join(" ");
+    const data = entities.encodeHTML(text);
 
-    const canvas = global.canvas.createCanvas(512, 512);
-
-    await qr.toCanvas(canvas, input, { margin: 2 });
+    const response = await fetch(`https://image-charts.com/chart?chs=150x150&cht=qr&chl=${data}`);
 
     return message.channel.send(new global.Discord.MessageEmbed({
-      color: colors.default,
+      color: global.guildInstance.config.colors.default,
       title: "QR Code",
-      description: input,
-      files: [ new global.Discord.MessageAttachment(canvas.toBuffer(), "qr.png") ],
-      image: { url: "attachment://qr.png" }
+      description: text,
+      image: { url: response.url },
+      footer: { iconURL: message.author.avatarURL(), text: `Requested by ${message.author.tag}` }
     }));
   }
 };
