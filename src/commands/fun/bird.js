93cefb427a0664174d9db527d8c408a2a10398ce
litleck <nwowens32@gmail.com>
2021-01-27 20:41:17 -0500

    Added bird command

---
+++ src/commands/fun/bird.js
@@ -0,0 +1,17 @@
+module.exports = {
+  description: "Get a random picture of a bird!",
+  aliases: [ "birb" ],
+  async execute (client, message) {
+    const { colors } = await global.sequelize.models.guildConfig.findByPk(message.guild.id);
+
+    const { link } = await util.fetchJSON("https://some-random-api.ml/img/birb");
+
+    return message.channel.send({ embed: {
+      color: colors.default,
+      title: "Random Bird",
+      url: link,
+      image: { url: link },
+      footer: { text: "Source: https://some-random-api.ml/img/birb" }
+    } });
+  }
+};
