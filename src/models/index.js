27ba9ae0dff4d822c3f93f718d0ef9f12511b0ec
litleck <nwowens32@gmail.com>
2021-05-05 22:14:11 -0400

    Update associations

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,4 @@
 const Sequelize = require("sequelize");
-const chalk = require("chalk");
 
 exports.create = async () => {
   const sequelize = new Sequelize("database", "user", "password", {
@@ -17,17 +16,16 @@ exports.create = async () => {
   const Guild = require("./Guild.js")(sequelize, Sequelize);
   const GuildConfig = require("./GuildConfig.js")(sequelize, Sequelize);
 
-  const Member = require("./Member.js")(sequelize, Sequelize);
-  const MemberConfig = require("./MemberConfig.js")(sequelize, Sequelize);
+  const User = require("./User.js")(sequelize, Sequelize);
+  const UserConfig = require("./UserConfig.js")(sequelize, Sequelize);
 
   const Command = require("./Command.js")(sequelize, Sequelize);
 
   // Set up associations
   Guild.hasOne(GuildConfig, { as: "config", foreignKey: "guildId" });
-  Member.hasOne(MemberConfig, { as: "config", foreignKey: "userId" } );
+  User.hasOne(UserConfig, { as: "config", foreignKey: "userId" } );
 
-  Guild.hasMany(Command);
-  Guild.hasMany(Member);
+  Guild.hasMany(Command, { as: "commands" });
 
   await sequelize.sync();
 
@@ -39,20 +37,4 @@ exports.initialize = async (sequelize, guild) => {
   await sequelize.models.guildConfig.findOrCreate({ where: { guildId: guild.id } });
 
   return guildInstance;
-};
-
-exports.checkMembers = async (guild, guildInstance) => {
-  const databaseMembers = guildInstance.getMembers();
-
-  if (!databaseMembers) return;
-
-  for (const memberData of await databaseMembers.findAll()) {
-    try {
-      await guild.members.fetch(memberData.id);
-    } catch (error) {
-      await memberData.destroy();
-
-      console.log(chalk.green(`(${util.time()})`), `Deleted ${memberData.id} from ${guild.name}.`);
-    }
-  }
 };
