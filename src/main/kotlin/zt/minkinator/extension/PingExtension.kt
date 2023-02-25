638042f0e78752d55de54e590fd82ce17c375eca
zt64 <nwowens32@gmail.com>
2023-02-25 14:31:48 -0500

    Fix ping extension

--- src/main/kotlin/zt/minkinator/extension/PingExtension.kt
+++ src/main/kotlin/zt/minkinator/extension/PingExtension.kt
@@ -2,7 +2,10 @@ package zt.minkinator.extension
 
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.types.respond
+import dev.kord.common.Color
+import dev.kord.rest.builder.message.create.embed
 import zt.minkinator.util.publicSlashCommand
+import zt.minkinator.util.success
 
 object PingExtension : Extension() {
     override val name = "ping"
@@ -14,11 +17,11 @@ object PingExtension : Extension() {
         ) {
             action {
                 respond {
-//                    embed {
-//                        color = Color.success
-//                        title = "Ping"
-//                        description = this@PingExtension.kord.gateway.averagePing?.toString() ?: "Unknown"
-//                    }
+                    embed {
+                        color = Color.success
+                        title = "Ping"
+                        description = this@PingExtension.kord.gateway.averagePing?.toString() ?: "Unknown"
+                    }
                 }
             }
         }
